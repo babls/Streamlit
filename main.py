c1460c110319638525283fea9d5205b69eb0021d
Saakian Daniil <daniil_sa@pspu.ru>
2023-07-14 17:32:14 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from io import StringIO
+
+st.title('Hello world')
+st.markdown("---")
+uploaded_files = st.file_uploader("Choose a CSV file",type=["csv"], accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    if uploaded_file is not None:
+        st.markdown("---")
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+        # To read file as string:
+        #   string_data = stringio.read()
+        #   st.write(string_data)
+
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file, sep=';')
+        st.table(dataframe)
